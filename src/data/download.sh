ce1ce027dc15e1128c9f90fde0e9944ad1069976
KhalidCK <fr.ckhalid@gmail.com>
2018-03-25 20:00:27 +0200

    retrieve stif data for expected schedule

--- src/data/download.sh
+++ src/data/download.sh
@@ -8,6 +8,7 @@ profil_validation2='/validations-sur-le-reseau-ferre-profils-horaires-par-jour-t
 profil_nb1='/validations-sur-le-reseau-ferre-nombre-de-validations-par-jour-1er-sem/'
 profil_nb2='/validations-sur-le-reseau-ferre-nombre-de-validations-par-jour-2e-sem/'
 lignes='/liste-arrets-lignes-tc-idf/download/?format=json&refine.agency_name=METRO&timezone=Europe/Berlin'
+schedule='/offre-horaires-tc-gtfs-idf/files/f24cf9dbf6f80c28b8edfdd99ea16aad/download/'
 #download only data regarding metro network (res = 110)
 option='download/?format=json&refine.code_stif_res=110&timezone=Europe/Berlin'
 wget -O "$output"/nb-validation_S1-2017.json "$base_url""$profil_nb1""$option" -
@@ -15,3 +16,5 @@ wget -O "$output"/nb-validation_S2-2017.json "$base_url""$profil_nb2""$option" -
 wget -O "$output"/profil-validation_S1-2017.json "$base_url""$profil_validation1""$option" 
 wget -O "$output"/profil-validation_S2-2017.json "$base_url""$profil_validation2""$option"
 wget -O "$output"/lignes.json "$base_url""$lignes"
+wget -O "$output"/gtfs.zip "$base_url""$schedule"
+unzip "$output"/gtfs.zip -d $output
