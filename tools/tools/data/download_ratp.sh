3e09239e76e7b138b2ea5aa668ba6d8b78099246
KhalidCK <fr.ckhalid@gmail.com>
2018-04-01 10:09:34 +0200

    clean

--- tools/tools/data/download_ratp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-output='data/raw'
-mkdir -p $output
-echo 'Download data from ratp website'
-wget -O $output/ratp-trafic-2016.json 'https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2016/download/?format=json&refine.reseau=M%C3%A9tro&timezone=Europe/Berlin'
